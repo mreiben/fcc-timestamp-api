eb4a67363c07162c9c48f40fc1db014145c39a13
mreiben <jason.eiben@gmail.com>
2016-12-31 23:23:00 +0000

    fixed unix date parsing

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
 
-app.get('/', function(req, res){
-    res.send("Hello, world!");
+// app.get('/', function(req, res){
+//     res.send("Hello, world!");
+// });
+
+app.get('/:date', function(req, res){
+    //create response object
+    var obj = {
+        unix: "null",
+        natural: "null"
+    }
+    
+    //get date as string
+    var d = req.params.date;
+    if(moment.unix(parseInt(d)).isValid()){
+        obj.unix = parseInt(d);
+        obj.natural = new moment(parseInt(d)).format("MMMM Do, YYYY");
+    } else if (moment(d).isValid()){
+        obj.unix = moment(d).unix();
+        obj.natural = moment(d).format("MMMM Do, YYYY");
+    }
+    
+    //render response object
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(obj));
 });
 
-app.listen(8080, function(){
-    console.log("Listening on port 8080");
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, function() {
+    console.log('Listening on port %d', server_port);
 });
